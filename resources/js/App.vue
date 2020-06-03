acc3dc51193458b736fe0c026d9770843becee90
meinjam <injam.bd.jsr@gmail.com>
2020-06-03 11:46:06 +0600

    vuetify and single page templete added

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,60 @@
 <template>
-    <div>
-        <h1>This is a vue component.</h1>
-        <router-link :to="{ name: 'Home' }" class="btn btn-primary"
-            >Add</router-link
-        >
-        <router-view />
-    </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboards</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-email</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Contact</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="blue" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>LaraVue Spa Blog</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col class="text-center">
+            <v-tooltip left>
+              <template v-slot:activator="{ on }">
+                <v-btn :href="source" icon large target="_blank" v-on="on">
+                  <v-icon large>mdi-code-tags</v-icon>
+                </v-btn>
+              </template>
+              <span>Source</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+    <v-footer color="blue" app>
+      <span class="white--text">Copyright &copy; 2020. All rights reserved</span>
+    </v-footer>
+  </v-app>
 </template>
+
+<script>
+export default {
+  props: {
+    source: String
+  },
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,16 @@
 require("./bootstrap");
 window.Vue = require("vue");
 import router from "./router";
-import ViewUI from "view-design";
-import "view-design/dist/styles/iview.css";
+// import ViewUI from "view-design";
+// import "view-design/dist/styles/iview.css";
+import vuetify from "./vuetify/index";
 
-Vue.use(ViewUI);
+// Vue.use(ViewUI);
 
 Vue.component("mainapp", require("./App.vue").default);
 
 const app = new Vue({
+    vuetify,
     el: "#app",
     router
 });
